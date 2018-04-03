8069fd1b796a5277d488215f4cfe920c0f06a4b9
jpcortez <jpcortez@miuandes.cl>
2018-04-03 17:21:28 -0300

    Second commit for april 3 lab

---
+++ ConsoleApp8/Licencia.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Licencia
+    {
+        public List<string> licencia;
+        public string rut;
+
+
+
+        public void AddLicencia(string rut)
+        {
+            licencia.Add(rut);
+        }
+        public bool RevisarLicencia(Persona persona)
+        {
+            foreach (string i in licencia)
+            {
+                if (persona.rut == i)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp8/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class Persona : Cliente
+    {
+        public string nombre, rut;
+
+        public Persona(string nombre, string rut)
+        {
+            this.nombre = nombre;
+            this.rut = rut;
+        }
+
+        
+    }
+}
